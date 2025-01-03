ade5663a6b9cc8023ac53bb6057184bcbbf7d821
hkaur108 <hkaur108@hotmail.com>
2025-01-02 20:25:08 -0500

    fixed scss and jquery

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-var mediaQuery = window.matchMedia('(max-width: 600px)');
-var widerScreen = window.matchMedia('(min-width: 601px)');
-
-
-$(function(){
-    $('.show-content').hide()
-    $('.primary-content').show();
-    $('.wider-show-content').hide();
-
-
-    $('.share-icon').on('click', ()=>{
-        if(mediaQuery.matches){
-            $('.show-content').toggle();
-            $('.primary-content').toggle();
-        }
-    else if(widerScreen.matches)
-        {
-        $('.primary-content').show();
-        $('.wider-show-content').toggle();
-
-    }}
-)})
-
+$(document).ready(()=>{
+    $('.share-icon').on('click',()=>{
+        $('.show-content').css(
+            {'opacity':'1'}
+        );
+    })
+})
