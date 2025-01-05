16218b0c2212d8c8e7733586af037ae8753498e6
hkaur108 <hkaur108@hotmail.com>
2025-01-04 20:07:01 -0500

    fixed styling

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 $(document).ready(()=>{
-    $('.share-icon').on('click',()=>{
-        $('.show-content').css(
-            {'opacity':'1'}
-        );
+    let showContent=$('.show-content');
+    let smallScreen=$('.smaller-screen');
+    if($(window).width() > 650){
+        $('.wider-screen').css({
+            'opacity':'1'
+        },'slow')
+        $('.share-icon').on('click',()=>{
+                showContent.toggleClass('show')
+        })}
+        else{
+            $('.share-icon').on('click',()=>{ 
+                console.log('I am getting clicked') 
+                $('.smaller-screen').remove() 
+                // $('.smaller-screen').css({
+                //     'opacity':'0'
+                // });
+                $('.wider-show-content').css({
+                    'opacity':'1'
+                })
+                $('.wider-show-content').children('.share-icon').on('click',()=>{
+                    $('.wider-show-content').css({
+                        'opacity':'0'
+                    });
+                    $('.bottom-container').append(smallScreen);
+                    // $('.smaller-screen').css({
+                    //     'opacity':'1'
+                    // });
+                });
+            })
+            
+
+        }
+       
     })
-})
+
